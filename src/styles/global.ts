0ccd88b784d20c86617f8ca0fe28a831766f67ad
Renan <renan.sergipe68@gmail.com>
2021-10-05 20:54:16 -0300

    Alterando as fonts do projeto e crinado componente header

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -24,8 +24,18 @@ body{
   -webkit-font-smoothing: antialiased;
 }
 
+body, textarea, input, button{
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+}
+
+h1, h2, h3, h4, h5, h6, strong{
+  font-weight: 600;
+}
+
 button{
   cursor: pointer;
+  color: var(--shape)
 }
 [disabled]{
   opacity: 0.6;
