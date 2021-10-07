e53819e9f81cc6c654dbc348138387ea56539eb3
Renan <renan.sergipe68@gmail.com>
2021-10-07 20:57:34 -0300

    Componente Transactions

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { Transactions } from "../Transactions";
 import { Container } from "./style";
 
 export function Dashboard(){
   return(
     <Container>
       <Summary/>
+      <Transactions/>
     </Container>
   )
 }
